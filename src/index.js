dcb7fdda4114b943c629ecffd4780255d2b2860a
SujalShah3234 <shahc9437@gmail.com>
2022-03-04 18:28:19 +0530

    💄 Custom theme integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.scss';
 
+const theme = createTheme({
+  palette: {
+    success: {
+      main: '#21d07a',
+    },
+    warning: {
+      main: '#d2d531',
+    },
+    error: {
+      main: '#da3e63',
+    },
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
