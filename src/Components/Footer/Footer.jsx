3b31f406d5b6d4db9e507630d3d794d98b46f9d8
SujalShah3234 <shahc9437@gmail.com>
2022-03-08 14:19:25 +0530

    ✨ Add: Stable footer section

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,47 @@
+import { Button } from '@mui/material';
+import React from 'react';
+
+import styles from './Footer.module.scss';
+
+const Footer = () => {
+  return (
+    <section className={styles['footer-section']}>
+      <div className={styles['content-wrapper']}>
+        <div className={styles['column']}>
+          <div className={styles['column-header']}>
+            <h2>Join Today</h2>
+          </div>
+          <div className={styles['column-content']}>
+            <div className={styles['column']}>
+              <p>
+                Get access to maintain your own <em>custom personal lists</em>,{' '}
+                <em>track what you've seen</em> and search and filter for{' '}
+                <em>what to watch next</em>—regardless if it's in theatres, on
+                TV or available on popular streaming services like Netflix,
+                Hotstar, and Amazon Prime Video.
+              </p>
+              <Button variant='contained' color='secondary' className={styles['sign-up-btn']}>
+                Sign Up
+              </Button>
+            </div>
+            <div className={styles['column']}>
+              <ul>
+                <li>Enjoy TMDB ad free</li>
+                <li>Maintain a personal watchlist</li>
+                <li>
+                  Filter by your subscribed streaming services and find
+                  something to watch
+                </li>
+                <li>Log the movies and TV shows you've seen</li>
+                <li>Build custom lists</li>
+                <li>Contribute to and improve our database</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
