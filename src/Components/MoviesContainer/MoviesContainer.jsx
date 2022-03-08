b6bd1f91bb7a0e4e01577972d773fa0ae7c6933f
SujalShah3234 <shahc9437@gmail.com>
2022-03-08 15:12:12 +0530

    💄 Stable tabs custom tabs component

--- src/Components/MoviesContainer/MoviesContainer.jsx
+++ src/Components/MoviesContainer/MoviesContainer.jsx
@@ -2,36 +2,51 @@ import { useEffect, useState } from 'react';
 import { API, BASE_URL } from '../../Constants';
 import MovieCard from '../MovieCard/MovieCard';
 import { SkeletonLoader } from '../MovieCard/SkeletonLoader';
+import CustomTab from '../Tabs/Tabs';
 import Title from '../Title/Title';
 import styles from './MoviesContainer.module.scss';
 
-const MoviesContainer = ({ category, mediaType, time = '', title = '' }) => {
+const MoviesContainer = ({
+  category,
+  mediaType,
+  tabs,
+  time = '',
+  title = '',
+}) => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [tabValue, setTabValue] = useState((tabs && tabs[0]) || '');
 
-  const fetchMovies = async () => {
-    try {
-      const res = await fetch(
-        `${BASE_URL}/${category}/${mediaType}/${time}?api_key=${API}`
-      );
-      const data = await res.json();
-      setMovies(data.results);
-      console.table(data.results);
-    } catch (error) {
-      alert(error.message);
-    } finally {
-      setLoading(false);
-    }
+  const handleChange = (event, tabValue) => {
+    setTabValue(tabValue);
+    console.log(tabValue);
   };
 
   useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const res = await fetch(
+          `${BASE_URL}/${category}/${mediaType}/${time}?api_key=${API}`
+        );
+        const data = await res.json();
+        setMovies(data.results);
+        console.table(data.results);
+      } catch (error) {
+        alert(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
     fetchMovies();
-  }, []);
+  }, [category, mediaType, time]);
 
   return (
     <section className={styles.scroll__wrap}>
-      <div>
+      <div className={styles['movie-header-container']}>
         <Title>{title}</Title>
+        {tabs && (
+          <CustomTab tabs={tabs} handleChange={handleChange} value={tabValue} />
+        )}
       </div>
       <div className={styles.inner__column}>
         {loading && [...Array(10)].map((_, i) => <SkeletonLoader key={i} />)}
--- src/Components/Tabs/Tabs.jsx
+++ src/Components/Tabs/Tabs.jsx
@@ -1,52 +1,33 @@
 import { styled } from '@mui/system';
 import TabsUnstyled from '@mui/base/TabsUnstyled';
 import TabsListUnstyled from '@mui/base/TabsListUnstyled';
-import TabPanelUnstyled from '@mui/base/TabPanelUnstyled';
 import { buttonUnstyledClasses } from '@mui/base/ButtonUnstyled';
 import TabUnstyled, { tabUnstyledClasses } from '@mui/base/TabUnstyled';
-
-const blue = {
-  50: '#F0F7FF',
-  100: '#C2E0FF',
-  200: '#80BFFF',
-  300: '#66B2FF',
-  400: '#3399FF',
-  500: '#007FFF',
-  600: '#0072E5',
-  700: '#0059B2',
-  800: '#004C99',
-  900: '#003A75',
-};
+import PropTypes from 'prop-types';
 
 const Tab = styled(TabUnstyled)`
   font-family: IBM Plex Sans, sans-serif;
-  color: white;
+  color: rgb(3, 37, 65);
   cursor: pointer;
   font-size: 0.875rem;
   font-weight: bold;
+  height: 30px;
   background-color: transparent;
   width: 100%;
-  padding: 12px 16px;
-  margin: 6px 6px;
+  padding: 7px 16px;
   border: none;
-  border-radius: 5px;
+  border-radius: 40px;
   display: flex;
   justify-content: center;
 
-  &:hover {
-    background-color: ${blue[400]};
-  }
-
-  &:focus {
-    color: #fff;
-    border-radius: 3px;
-    outline: 2px solid ${blue[200]};
-    outline-offset: 2px;
-  }
-
   &.${tabUnstyledClasses.selected} {
-    background-color: ${blue[50]};
-    color: ${blue[600]};
+    background-color: rgb(3, 37, 65);
+
+    span {
+      background: linear-gradient(to right, #c0fecf 0%, #1ed5a9 100%);
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+    }
   }
 
   &.${buttonUnstyledClasses.disabled} {
@@ -55,34 +36,42 @@ const Tab = styled(TabUnstyled)`
   }
 `;
 
-const TabPanel = styled(TabPanelUnstyled)`
-  width: 100%;
-  font-family: IBM Plex Sans, sans-serif;
-  font-size: 0.875rem;
-`;
-
 const TabsList = styled(TabsListUnstyled)`
-  min-width: 320px;
-  background-color: ${blue[500]};
-  border-radius: 8px;
+  width: 120%;
+  background-color: transparent;
+  border-radius: 40px;
   margin-bottom: 16px;
   display: flex;
+  height: 30px;
   align-items: center;
   justify-content: center;
   align-content: space-between;
+  border: 1px solid rgb(3, 37, 65);
 `;
-
-export default function TabsCustomized() {
+function CustomTab({ tabs, value, handleChange }) {
   return (
-    <TabsUnstyled defaultValue={0}>
+    <TabsUnstyled defaultValue={0} value={value} onChange={handleChange}>
       <TabsList>
-        <Tab>One</Tab>
-        <Tab>Two</Tab>
-        <Tab>Three</Tab>
+        {tabs.map((tab) => (
+          <Tab key={tab} value={tab}>
+            <span>{tab}</span>
+          </Tab>
+        ))}
       </TabsList>
-      <TabPanel value={0}>First content</TabPanel>
-      <TabPanel value={1}>Second content</TabPanel>
-      <TabPanel value={2}>Third content</TabPanel>
     </TabsUnstyled>
   );
 }
+
+CustomTab.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.string),
+  value: PropTypes.string,
+  handleChange: PropTypes.func,
+};
+
+CustomTab.defaultProps = {
+  tabs: [],
+  value: '',
+  handleChange: () => {},
+};
+
+export default CustomTab;
