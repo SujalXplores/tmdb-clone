b70a6dbbaff5d231d8562c8dd8e9dab6f120f51f
SujalShah3234 <shahc9437@gmail.com>
2022-03-08 18:37:31 +0530

    💄 Stable header UI

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,6 +6,7 @@ import {
   IconButton,
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import PropTypes from 'prop-types';
 
 import styles from './Header.module.scss';
 
@@ -22,6 +23,11 @@ function HideOnScroll(props) {
   );
 }
 
+Header.propTypes = {
+  children: PropTypes.node,
+  window: PropTypes.func,
+};
+
 export default function Header(props) {
   return (
     <HideOnScroll {...props}>
@@ -35,20 +41,27 @@ export default function Header(props) {
           >
             <MenuIcon />
           </IconButton>
-          <div>
-            <img
-              src='https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg'
-              alt='The Movie Database (TMDB)'
-              width='154'
-              height='20'
-            />
+          <div className={styles['header-container']}>
+            <div className={styles['inner-nav']}>
+              <img
+                src='https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg'
+                alt='The Movie Database (TMDB)'
+                width='154'
+                height='20'
+              />
+              <ul className={styles['nav-items']}>
+                <li className={styles['nav-item']}>Movies</li>
+                <li className={styles['nav-item']}>TV Shows</li>
+              </ul>
+            </div>
+            <ul className={styles['nav-items']}>
+              <li className={styles['nav-item']}>Login</li>
+              <li className={styles['nav-item']}>Join TMDB</li>
+              <li className={styles['nav-item']}>
+                <span className={styles.search}></span>
+              </li>
+            </ul>
           </div>
-          <ul className={styles['nav-items']}>
-            <li className={styles['nav-item']}>Movies</li>
-            <li className={styles['nav-item']}>TV Shows</li>
-            <li className={styles['nav-item']}>People</li>
-            <li className={styles['nav-item']}>More</li>
-          </ul>
         </Toolbar>
       </AppBar>
     </HideOnScroll>
