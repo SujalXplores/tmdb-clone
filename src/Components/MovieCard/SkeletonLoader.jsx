a19dc974368b5d7cc801e41018fb243788760d08
SujalShah3234 <shahc9437@gmail.com>
2022-03-09 12:01:31 +0530

    💄 UI & UX changes

--- src/Components/MovieCard/SkeletonLoader.jsx
+++ src/Components/MovieCard/SkeletonLoader.jsx
@@ -5,22 +5,25 @@ export const SkeletonLoader = () => {
   return (
     <div className={styles.movie__card}>
       <div className={styles['movie__img-container']}>
-        <div className={styles.image}>
-          <div className={styles.wrapper}>
-            <Skeleton variant='rectangular' width='100%' height='100%' />
-          </div>
+        <div className={styles.wrapper}>
+          <Skeleton
+            animation='wave'
+            variant='rectangular'
+            width='100%'
+            height='100%'
+          />
         </div>
       </div>
       <div className={styles.movie__content}>
         <div className={styles.rating}>
-          <Skeleton variant='circular' width={40} height={40} />
+          <Skeleton
+            animation='wave'
+            variant='circular'
+            width={40}
+            height={40}
+          />
         </div>
-        <Skeleton
-          animation='wave'
-          height={10}
-          style={{ mb: 6 }}
-          width='100%'
-        />
+        <Skeleton animation='wave' height={10} sx={{ mb: 6 }} width='100%' />
         <Skeleton animation='wave' height={10} width='60%' />
       </div>
     </div>
