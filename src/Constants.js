e7b2fc2a9b862292940dc38dc304f197016e94cc
SujalShah3234 <shahc9437@gmail.com>
2022-03-11 14:26:23 +0530

    📝 Change in image url API

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,4 @@
-export const IMAGE_URL = 'https://www.themoviedb.org/t/p/w220_and_h330_face';
+export const IMAGE_URL = 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2';
 export const API = process.env.REACT_APP_API_KEY;
 
 export const BASE_URL = 'https://api.themoviedb.org/3';
