6b79601bb5b5f6c1207fe76c14454ef9f5c430cd
SujalShah3234 <shahc9437@gmail.com>
2022-03-15 10:53:09 +0530

    🔍  search query in home banner

--- src/Components/HomeBanner/HomeBanner.jsx
+++ src/Components/HomeBanner/HomeBanner.jsx
@@ -1,6 +1,18 @@
+import { useState } from 'react';
 import styles from './HomeBanner.module.scss';
 
 const HomeBanner = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const onSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log(searchQuery);
+  };
+
   return (
     <section className={styles['banner-container']}>
       <div className={styles['media']}>
@@ -25,9 +37,11 @@ const HomeBanner = () => {
                     autoComplete='off'
                     spellCheck='false'
                     placeholder='Search for a movie, tv show, person......'
+                    value={searchQuery}
+                    onChange={onSearchChange}
                   />
                 </label>
-                <input type='submit' />
+                <button type='submit' onClick={handleSearch}>Search</button>
               </form>
             </div>
           </div>
--- src/Constants.js
+++ src/Constants.js
@@ -1,12 +1,15 @@
 export const API = process.env.REACT_APP_API_KEY;
-export const IMAGE_URL = 'https://www.themoviedb.org/t/p/w220_and_h330_face';
-export const POSTER_URL = 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2';
-export const BACKDROP_URL = 'https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces';
-export const STREAMING_URL = 'https://www.themoviedb.org/t/p/original';
+export const BASE_URL = 'https://www.themoviedb.org/t/p';
+export const IMAGE_URL = `${BASE_URL}/w220_and_h330_face`;
+export const POSTER_URL = `${BASE_URL}/w300_and_h450_bestv2`;
+export const BACKDROP_URL = `${BASE_URL}/w1920_and_h800_multi_faces`;
+export const STREAMING_URL = `${BASE_URL}/original`;
+export const CAST_URL = `${BASE_URL}/w138_and_h175_face`;
 
-export const BASE_URL = 'https://api.themoviedb.org/3';
-export const TRENDING_DAY = `${BASE_URL}/trending/all/day?api_key=${API}`;
-export const TRENDING_WEEK = `${BASE_URL}/trending/all/week?api_key=${API}`;
-export const POPULAR_ON_TV = `${BASE_URL}/tv/popular?api_key=${API}`;
-export const IN_THEATERS = `${BASE_URL}/movie/now_playing?api_key=${API}`;
-export const SEARCH_URL = `${BASE_URL}/search/multi?api_key=${API}&query=`
+export const API_URL = 'https://api.themoviedb.org/3';
+export const TRENDING_DAY = `${API_URL}/trending/all/day?api_key=${API}`;
+export const TRENDING_WEEK = `${API_URL}/trending/all/week?api_key=${API}`;
+export const POPULAR_ON_TV = `${API_URL}/tv/popular?api_key=${API}`;
+export const IN_THEATERS = `${API_URL}/movie/now_playing?api_key=${API}`;
+
+export const SEARCH_URL = `${API_URL}/search/multi?api_key=${API}&query=`
