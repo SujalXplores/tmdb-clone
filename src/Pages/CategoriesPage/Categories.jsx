0513d9845382727a94b57721db5c591bb9b09c97
SujalShah3234 <shahc9437@gmail.com>
2022-03-23 23:09:23 +0530

    🚧 Custom accordion + minor UI fixes

--- src/Pages/CategoriesPage/Categories.jsx
+++ src/Pages/CategoriesPage/Categories.jsx
@@ -1,24 +1,12 @@
-import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import {
-  Accordion,
-  AccordionSummary,
-  AccordionDetails,
-  Typography,
-} from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import CustomAccordion from './CustomAccordion';
 
 import styles from './Categories.module.scss';
 import CategoryCard from './Category-Card';
 
 const Categories = () => {
   const params = useParams();
-  const [expanded, setExpanded] = useState(false);
   const { type, category } = params;
-
-  const handleChange = (panel) => (event, isExpanded) => {
-    setExpanded(isExpanded ? panel : false);
-  };
 
   console.log('PARAMS:', params);
   return (
@@ -34,54 +22,13 @@ const Categories = () => {
               </div>
               <div className={styles.content}>
                 <div className={styles.filter_container}>
-                  <Accordion
-                    expanded={expanded === 'panel1'}
-                    onChange={handleChange('panel1')}
-                    className={styles.accordion}
-                  >
-                    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                      <h2>Sort</h2>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                      <Typography>
-                        Nulla facilisi. Phasellus sollicitudin nulla et quam
-                        mattis feugiat. Aliquam eget maximus est, id dignissim
-                        quam.
-                      </Typography>
-                    </AccordionDetails>
-                  </Accordion>
-                  <Accordion
-                    expanded={expanded === 'panel2'}
-                    onChange={handleChange('panel2')}
-                    className={styles.accordion}
-                  >
-                    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                      <h2>Filters</h2>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                      <Typography>
-                        Donec placerat, lectus sed mattis semper, neque lectus
-                        feugiat lectus, varius pulvinar diam eros in elit.
-                        Pellentesque convallis laoreet laoreet.
-                      </Typography>
-                    </AccordionDetails>
-                  </Accordion>
-                  <Accordion
-                    expanded={expanded === 'panel3'}
-                    onChange={handleChange('panel3')}
-                    className={styles.accordion}
-                  >
-                    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                      <h2>Where to watch</h2>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                      <Typography>
-                        Nunc vitae orci ultricies, auctor nunc in, volutpat
-                        nisl. Integer sit amet egestas eros, vitae egestas
-                        augue. Duis vel est augue.
-                      </Typography>
-                    </AccordionDetails>
-                  </Accordion>
+                  <CustomAccordion title='Sort'>
+                    Sorting dropdown
+                  </CustomAccordion>
+                  <CustomAccordion title='Filters'>Filters</CustomAccordion>
+                  <CustomAccordion title='Where To Watch'>
+                    Where to watch
+                  </CustomAccordion>
                 </div>
                 <div>
                   <div className={styles.right_media_container}>
