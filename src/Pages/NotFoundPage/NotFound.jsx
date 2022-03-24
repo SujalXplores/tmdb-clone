eda623b70bff0c0287b50f2c641266699e326e31
SujalShah3234 <shahc9437@gmail.com>
2022-03-24 12:44:26 +0530

    💄 Not found page UI fix

--- src/Pages/NotFoundPage/NotFound.jsx
+++ src/Pages/NotFoundPage/NotFound.jsx
@@ -1,9 +1,20 @@
+import { Link } from 'react-router-dom';
 import styles from './NotFound.module.scss';
 
 export const NotFound = () => {
   return (
-    <div className={styles['not-found-container']}>
-      <h1>Oops! We can't find the page you're looking for</h1>
+    <div className={styles['column-wrapper']}>
+      <div className={styles['content-wrapper']}>
+        <div>
+          <div className={styles['error-wrapper']}>
+            <h2>Oops! We can't find the page you're looking for</h2>
+          </div>
+          <p>
+            You tried to request a page that doesn't exist. If you believe this
+            to be in error, let us know <Link to='/'>on the forums</Link>.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
