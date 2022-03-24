eb15e3fd64747053799785228eb2b85be59c24c1
SujalShah3234 <shahc9437@gmail.com>
2022-03-24 10:14:44 +0530

    📝 All tv and movie endpoints

--- src/Constants.js
+++ src/Constants.js
@@ -10,10 +10,19 @@ export const CAST_URL = `${BASE_URL}/w138_and_h175_face`;
 export const NETWORK_URL = `${BASE_URL}/h30`;
 export const SEASON_POSTER_URL = `${BASE_URL}/w130_and_h195_bestv2`;
 
+// Movie API endpoints
 export const API_URL = 'https://api.themoviedb.org/3';
 export const TRENDING_DAY = `${API_URL}/trending/all/day?api_key=${API}`;
 export const TRENDING_WEEK = `${API_URL}/trending/all/week?api_key=${API}`;
-export const POPULAR_ON_TV = `${API_URL}/tv/popular?api_key=${API}`;
 export const IN_THEATERS = `${API_URL}/movie/now_playing?api_key=${API}`;
+export const POPULAR_MOVIES = `${API_URL}/movie/popular?api_key=${API}`;
+export const UPCOMING_MOVIES = `${API_URL}/movie/upcoming?api_key=${API}`;
+export const TOP_RATED_MOVIES = `${API_URL}/movie/top_rated?api_key=${API}`;
+
+// TV API endpoints
+export const POPULAR_ON_TV = `${API_URL}/tv/popular?api_key=${API}`;
+export const TOP_RATED_ON_TV = `${API_URL}/tv/top_rated?api_key=${API}`;
+export const AIRING_TODAY_ON_TV = `${API_URL}/tv/airing_today?api_key=${API}`;
+export const TV_ON_THE_AIR = `${API_URL}/tv/on_the_air?api_key=${API}`;
 
 export const SEARCH_URL = `${API_URL}/search/multi?api_key=${API}&query=`;
