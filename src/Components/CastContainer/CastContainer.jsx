e11887490aaf9f26e5898a3c3df9e511203a05e3
SujalShah3234 <shahc9437@gmail.com>
2022-03-25 18:35:22 +0530

    ✨ minor optimizations

--- src/Components/CastContainer/CastContainer.jsx
+++ src/Components/CastContainer/CastContainer.jsx
@@ -48,6 +48,15 @@ export const CastContainer = ({ type, id, movieData }) => {
     fetchCast();
   }, [fetchCast]);
 
+  const handleScroll = (e) => {
+    const element = e.target;
+    if (element.scrollLeft > 25) {
+      element.style.setProperty('--opacity', 0);
+    } else {
+      element.style.setProperty('--opacity', 1);
+    }
+  };
+
   const currentSeason = movieData?.seasons?.at(-1);
 
   return (
@@ -56,163 +65,155 @@ export const CastContainer = ({ type, id, movieData }) => {
       <>
         <div className={styles.column_wrapper}>
           <div className={styles.content_wrapper}>
-            <div>
-              <div className={styles.white_column}>
-                <section className={styles.cast_section}>
-                  <h3 dir='auto'>
-                    {type === 'movie' ? 'Top Billed Cast' : 'Series Cast'}
-                  </h3>
-                  <div className={styles.scroll_fade}>
-                    <ol className={styles.people}>
-                      {castData.slice(0, 9).map((cast) => (
-                        <li className={styles.card} key={cast.id}>
-                          <Link to='/' className={styles.img_link}>
-                            {cast.profile_path ? (
-                              <div className={styles['img-container']}>
-                                <img
-                                  loading='lazy'
-                                  className={styles.profile}
-                                  src={`${CAST_URL}${cast.profile_path}`}
-                                  alt={cast.name}
-                                />
-                              </div>
-                            ) : cast.gender ? (
-                              <PersonFemale className={styles.no_image} />
-                            ) : (
-                              <PersonMale className={styles.no_image} />
-                            )}
-                          </Link>
-                          <p className={styles.name}>{cast.name}</p>
-                          <p className={styles.character}>{cast.character}</p>
-                        </li>
-                      ))}
-                    </ol>
-                    {!castData.length && (
-                      <span>
-                        We don't have any cast added to this Show. You can help
-                        by adding some!
-                      </span>
-                    )}
-                  </div>
-                </section>
-                {movieData.seasons && (
-                  <section className={styles.season_section}>
-                    <h3>Current Season</h3>
-                    <div className={styles.season_card}>
-                      <div className={styles.flex}>
-                        <div className={styles.poster}>
-                          <img
-                            className={`${
-                              !currentSeason.poster_path
-                                ? styles['fallback-poster']
-                                : ''
-                            }`}
-                            src={`${SEASON_POSTER_URL}${currentSeason.poster_path}`}
-                            alt='poster'
-                            loading='lazy'
-                            onError={(e) => (e.target.src = imageErrorSrc)}
-                          />
-                        </div>
-                        <div className={styles.content}>
-                          <div>
-                            <h2>Season {currentSeason.season_number}</h2>
-                            <h4>
-                              {currentSeason.air_date?.slice(0, 4) ||
-                                new Date().getFullYear()}{' '}
-                              | {currentSeason.episode_count} Episodes
-                            </h4>
-                            <div className={styles.season_overview}>
-                              <p>
-                                Season {currentSeason.season_number} of{' '}
-                                {movieData.name} premiered on{' '}
-                                {convertDate(currentSeason.air_date, 'long')}.
-                              </p>
+            <div className={styles.white_column}>
+              <section className={styles.cast_section}>
+                <h3 dir='auto'>
+                  {type === 'movie' ? 'Top Billed Cast' : 'Series Cast'}
+                </h3>
+                <div className={styles.scroll_fade}>
+                  <ol className={styles.people} onScroll={handleScroll}>
+                    {castData.slice(0, 9).map((cast) => (
+                      <li className={styles.card} key={cast.id}>
+                        <Link to='/' className={styles.img_link}>
+                          {cast.profile_path ? (
+                            <div className={styles['img-container']}>
+                              <img
+                                loading='lazy'
+                                className={styles.profile}
+                                src={`${CAST_URL}${cast.profile_path}`}
+                                alt={cast.name}
+                              />
                             </div>
+                          ) : cast.gender ? (
+                            <PersonFemale className={styles.no_image} />
+                          ) : (
+                            <PersonMale className={styles.no_image} />
+                          )}
+                        </Link>
+                        <p className={styles.name}>{cast.name}</p>
+                        <p className={styles.character}>{cast.character}</p>
+                      </li>
+                    ))}
+                  </ol>
+                  {!castData.length && (
+                    <span>
+                      We don't have any cast added to this Show. You can help by
+                      adding some!
+                    </span>
+                  )}
+                </div>
+              </section>
+              {movieData.seasons && (
+                <section className={styles.season_section}>
+                  <h3>Current Season</h3>
+                  <div className={styles.season_card}>
+                    <div className={styles.flex}>
+                      <div className={styles.poster}>
+                        <img
+                          className={`${
+                            !currentSeason.poster_path
+                              ? styles['fallback-poster']
+                              : ''
+                          }`}
+                          src={`${SEASON_POSTER_URL}${currentSeason.poster_path}`}
+                          alt='poster'
+                          loading='lazy'
+                          onError={(e) => (e.target.src = imageErrorSrc)}
+                        />
+                      </div>
+                      <div className={styles.content}>
+                        <div>
+                          <h2>Season {currentSeason.season_number}</h2>
+                          <h4>
+                            {currentSeason.air_date?.slice(0, 4) ||
+                              new Date().getFullYear()}{' '}
+                            | {currentSeason.episode_count} Episodes
+                          </h4>
+                          <div className={styles.season_overview}>
+                            <p>
+                              Season {currentSeason.season_number} of{' '}
+                              {movieData.name} premiered on{' '}
+                              {convertDate(currentSeason.air_date, 'long')}.
+                            </p>
                           </div>
                         </div>
                       </div>
                     </div>
-                  </section>
-                )}
-                <section className={styles.recommendations}>
-                  <div>
-                    <h3 dir='auto'>Recommendations</h3>
-                    <div className={styles.scroll_wrap}>
-                      {recommendations.length > 0 && (
-                        <div className={styles.scroll}>
-                          {recommendations.map((recommendation) => (
-                            <div
-                              className={styles.item}
-                              key={recommendation.id}
-                            >
-                              <div
-                                className={`${styles['img-container']} ${
-                                  !recommendation.backdrop_path
-                                    ? styles['no-image']
-                                    : ''
-                                }`}
-                              >
-                                <Link
-                                  to={`/${recommendation.media_type}/${recommendation.id}`}
-                                  title={
-                                    recommendation.name || recommendation.title
-                                  }
-                                >
-                                  <img
-                                    loading='lazy'
-                                    className={`${
-                                      !recommendation.backdrop_path
-                                        ? styles['fallback-poster']
-                                        : ''
-                                    }`}
-                                    src={`${RECOMMENDATIONS_BACKDROP_URL}${recommendation.backdrop_path}`}
-                                    alt={
-                                      recommendation.name ||
-                                      recommendation.title
-                                    }
-                                    onError={(e) =>
-                                      (e.target.src = imageErrorSrc)
-                                    }
-                                  />
-                                  <div className={styles.meta}>
-                                    <span className={styles.release_date}>
-                                      <span className={styles.calendar}></span>
-                                      {recommendation.release_date ||
-                                        recommendation.first_air_date}
-                                    </span>
-                                  </div>
-                                </Link>
-                              </div>
-                              <p className={styles.bottom_flex}>
-                                <Link
-                                  className={styles.title}
-                                  title={
-                                    recommendation.name || recommendation.title
-                                  }
-                                  to={`/${recommendation.media_type}/${recommendation.id}`}
-                                >
-                                  {recommendation.name || recommendation.title}
-                                </Link>
-                                <span className={styles.vote_average}>
-                                  {Math.round(recommendation.vote_average * 10)}
-                                  %
-                                </span>
-                              </p>
-                            </div>
-                          ))}
-                        </div>
-                      )}
-                      {recommendations.length === 0 && (
-                        <p>
-                          We don't have enough data to suggest any movies based
-                          on Deep Water. You can help by rating movies you've
-                          seen.
-                        </p>
-                      )}
-                    </div>
                   </div>
                 </section>
-              </div>
+              )}
+              <section className={styles.recommendations}>
+                <div>
+                  <h3 dir='auto'>Recommendations</h3>
+                  <div className={styles.scroll_wrap}>
+                    {recommendations.length > 0 && (
+                      <div className={styles.scroll}>
+                        {recommendations.map((recommendation) => (
+                          <div className={styles.item} key={recommendation.id}>
+                            <div
+                              className={`${styles['img-container']} ${
+                                !recommendation.backdrop_path
+                                  ? styles['no-image']
+                                  : ''
+                              }`}
+                            >
+                              <Link
+                                to={`/${recommendation.media_type}/${recommendation.id}`}
+                                title={
+                                  recommendation.name || recommendation.title
+                                }
+                              >
+                                <img
+                                  loading='lazy'
+                                  className={`${
+                                    !recommendation.backdrop_path
+                                      ? styles['fallback-poster']
+                                      : ''
+                                  }`}
+                                  src={`${RECOMMENDATIONS_BACKDROP_URL}${recommendation.backdrop_path}`}
+                                  alt={
+                                    recommendation.name || recommendation.title
+                                  }
+                                  onError={(e) =>
+                                    (e.target.src = imageErrorSrc)
+                                  }
+                                />
+                                <div className={styles.meta}>
+                                  <span className={styles.release_date}>
+                                    <span className={styles.calendar}></span>
+                                    {recommendation.release_date ||
+                                      recommendation.first_air_date}
+                                  </span>
+                                </div>
+                              </Link>
+                            </div>
+                            <p className={styles.bottom_flex}>
+                              <Link
+                                className={styles.title}
+                                title={
+                                  recommendation.name || recommendation.title
+                                }
+                                to={`/${recommendation.media_type}/${recommendation.id}`}
+                              >
+                                {recommendation.name || recommendation.title}
+                              </Link>
+                              <span className={styles.vote_average}>
+                                {Math.round(recommendation.vote_average * 10)}%
+                              </span>
+                            </p>
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                    {recommendations.length === 0 && (
+                      <p>
+                        We don't have enough data to suggest any movies based on
+                        Deep Water. You can help by rating movies you've seen.
+                      </p>
+                    )}
+                  </div>
+                </div>
+              </section>
             </div>
             <div className={styles.grey_column}>
               <div>
